e1716bbb9993423fe087b2cd0ca0d175685d671f
manish <manis.kasyap@gmail.com>
2017-11-15 20:00:27 +0400

    extracting links from header

--- src/home/home.component.js
+++ src/home/home.component.js
@@ -6,13 +6,17 @@ class Home extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			links: "",
 			houses: []
 		};
 	}
 
 	componentDidMount() {
 		fetch("https://www.anapioficeandfire.com/api/houses?pageSize=50")
-			.then(res => res.json())
+			.then(res => {
+				this.setState({ links: res.headers.get("link") });
+				return res.json();
+			})
 			.then(data => {
 				this.setState({ houses: data });
 			});
@@ -20,13 +24,15 @@ class Home extends Component {
 
 	render() {
 		const housesList = this.state.houses.map(house => (
-			<li key={house._id}>
+			<li key={house.url}>
 				<Link to="/details">{house.name}</Link>
 			</li>
 		));
 		return (
 			<div>
 				<p>all the houses</p>
+				<span>Links: </span>
+				<span>{this.state.links}</span>
 				<ul>{housesList}</ul>
 				<div id="details" />
 				<Route path="/details" component={HouseDetails} />
